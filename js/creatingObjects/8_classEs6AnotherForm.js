ed61f17c96edb6361fb8c1ee031429909f4e5cea
Artur Drummond <arturbdr@gmail.com>
2018-03-27 23:54:40 -0300

    - Added new way to create classes in ES6 (not standart way)

---
+++ js/creatingObjects/8_classEs6AnotherForm.js
@@ -0,0 +1,23 @@
+// Only work in ES6
+let Notebook = class {
+    constructor(price, type) {
+        this.price = price;
+        this.type = type;
+    }
+
+    printDetails() {
+        console.log(`The price of this computer is ${this.price} and the type is ${this.type}`);
+    }
+
+    get upperType() {
+        return this.type.toUpperCase();
+    }
+};
+
+const macBook = new Notebook('1.999', 'MacBook Pro');
+const dell = new Notebook('1.299', 'Dell Latitude');
+
+macBook.printDetails();
+dell.printDetails();
+
+console.log(macBook.upperType);
